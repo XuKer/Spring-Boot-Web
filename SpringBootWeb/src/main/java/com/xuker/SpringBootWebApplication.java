daeb0e20026f16e0d1af1975dae782cfa8e7276f
XuKer <wxk1035@gmail.com>
2019-01-23 15:53:39 +0800

    集成错误页面

--- SpringBootWeb/src/main/java/com/xuker/SpringBootWebApplication.java
+++ SpringBootWeb/src/main/java/com/xuker/SpringBootWebApplication.java
@@ -3,12 +3,14 @@ package com.xuker;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author XuKe
  */
-@SpringBootApplication
+@SpringBootApplication//(exclude = {ElasticsearchAutoConfiguration.class, ElasticsearchDataAutoConfiguration.class})
 @MapperScan("com.xuker.dao")
 public class SpringBootWebApplication extends SpringBootServletInitializer {
 
---
+++ SpringBootWeb/src/main/java/com/xuker/error/MyErrorPageConfig.java
@@ -0,0 +1,9 @@
+package com.xuker.error;
+
+/**
+ * @Author:XuKe
+ * @CreateOn:2019-01-23 11:23
+ * @Email ：wxk1035@gmail.com
+ */
+public class MyErrorPageConfig {
+}
